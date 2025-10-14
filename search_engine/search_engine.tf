bbbd5a648421075fad3bc3456babb1ec8870c557
Danila Solovenko <dansil2301@gmail.com>
2025-10-14 23:20:56 +0200

    feat: added PVC storage for search engine

--- search_engine/search_engine.tf
+++ search_engine/search_engine.tf
@@ -1,5 +1,10 @@
+resource "kubernetes_manifest" "search_engine_pvc" {
+  manifest = yamldecode(file("${path.module}/search_engine_pvc.yaml"))
+}
+
 resource "kubernetes_manifest" "search_engine_deployment" {
   manifest = yamldecode(file("${path.module}/search_engine_deployment.yaml"))
+  depends_on = [kubernetes_manifest.search_engine_pvc]
 }
 
 resource "kubernetes_manifest" "search_engine_service" {
