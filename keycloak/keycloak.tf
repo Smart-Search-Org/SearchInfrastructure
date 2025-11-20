50032b3d8a34d73bc4b265e4e098469639779d6d
Danila Solovenko <dansil2301@gmail.com>
2025-11-21 00:34:02 +0100

    feat: new leycloak structure

--- keycloak/keycloak.tf
+++
@@ -1,7 +0,0 @@
-resource "kubernetes_manifest" "keycloak_deployment" {
-  manifest = yamldecode(file("${path.module}/keycloak_deployment.yaml"))
-}
-
-resource "kubernetes_manifest" "keycloak_service" {
-  manifest = yamldecode(file("${path.module}/keycloak_service.yaml"))
-}
